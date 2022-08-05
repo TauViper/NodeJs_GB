908b09886f639655d22c1b470fe63b9fc62a1d29
oraz <orazka79@gmail.com>
2022-08-05 13:46:53 +0300

    "Lesson1 for check"

--- Beep.js
+++ Beep.js
@@ -1,6 +1,24 @@
 
-import beep from 'beepbeep';
-const beepbeep = require("beep");
+import ansi from 'ansi';
+import beepbeep from 'beepbeep';
+import clicolor from 'cli-color';
 
+const cursor = ansi(process.stdout);
+
+
+cursor
+    .red()
+    .bg.grey()
+    .write("Hello World!")
+    .bg.reset()
+    .write("\n");
 
 beepbeep(3, 1000);
+
+console.log(clicolor.red("Text in red"));
+
+
+console.log(clicolor.yellow("Text in yellow"));
+
+
+console.log(clicolor.green("Text in green"));
